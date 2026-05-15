101e9be9f75672b0d4f7183772a759840edebf18
nayla saskia zallianti <naylasaskiazalianti@gmail.com>
2026-05-15 13:46:54 +0700

    tamu

--- HostelManagement_2_16/src/Model/Tamu.java
+++ HostelManagement_2_16/src/Model/Tamu.java
@@ -2,12 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
-/**
- *
- * @author user
- */
+import java.sql.Date;
+
 public class Tamu {
+    private int id;
+    private String nama;
+    private String noKtp;
+    private String noTelepon;
+    private String alamat;
+    private Date tanggalLahir;
     
+    public Tamu() {}
+    
+    public Tamu(String nama, String noKtp, String noTelepon, String alamat, Date tanggalLahir) {
+        this.nama = nama;
+        this.noKtp = noKtp;
+        this.noTelepon = noTelepon;
+        this.alamat = alamat;
+        this.tanggalLahir = tanggalLahir;
+    }
+    
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+    public String getNoKtp() { return noKtp; }
+    public void setNoKtp(String noKtp) { this.noKtp = noKtp; }
+    public String getNoTelepon() { return noTelepon; }
+    public void setNoTelepon(String noTelepon) { this.noTelepon = noTelepon; }
+    public String getAlamat() { return alamat; }
+    public void setAlamat(String alamat) { this.alamat = alamat; }
+    public Date getTanggalLahir() { return tanggalLahir; }
+    public void setTanggalLahir(Date tanggalLahir) { this.tanggalLahir = tanggalLahir; }
 }
